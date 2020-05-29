45e103bc458d27eb186560b3eec574bb249e493d
Gracious <14353802+GraciousGpal@users.noreply.github.com>
2020-05-29 17:30:24 +0100

    - Fixed a bug where the entire frame was included in the mean calculation. - Now it only takes in the normal prices for calculating the average.

--- cogs/market.py
+++ cogs/market.py
@@ -87,7 +87,7 @@ class Market(commands.Cog):
             avg_price.append(int(h_data[1][city]['avg_price'].mean()))
             avg_sell_volume.append((city, int(h_data[1][city]['item_count'].mean())))
 
-        avg_current_price = current_prices[1].mean()
+        avg_current_price = current_prices[1].loc['Normal', :]
 
         avg_p = self.c_game_currency(int(self.average(avg_price)))
         avg_cp = self.c_game_currency(int(self.average(avg_current_price)))
