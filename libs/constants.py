be5b79819672ccd3600b8b1b313b2f7113a92378
GraciousGPal <14353802+GraciousGpal@users.noreply.github.com>
2021-05-02 18:06:28 +0100

    - Added Logging to Most of the modules and libs. - Corrected Logging, so that it outputs to console as well as discord.log. - Huge Code cleanup, a lot of code has been separated into their own files. - Moved optimized_libs to their own folder for folder cleanliness. - Added all the necessary packages to requirements.txt

---
+++ libs/constants.py
@@ -0,0 +1,46 @@
+# ------- PACKAGE CONSTANTS ------- #
+
+
+BASE_URL_HISTORY = "https://www.albion-online-data.com/api/v2/stats/history/"
+BASE_URL_CURRENT = "https://www.albion-online-data.com/api/v2/stats/prices/"
+LOCATIONS = [
+    "Thetford",
+    "Martlock",
+    "Caerleon",
+    "Lymhurst",
+    "Bridgewatch",
+    "FortSterling",
+    "ArthursRest",
+    "MerlynsRest",
+    "MorganasRest",
+    "BlackMarket",
+]
+TIERS = [
+    "Beginner's",
+    "Novice's",
+    "Journeyman's",
+    "Adept's",
+    "Expert's",
+    "Master's",
+    "Grandmaster's",
+    "Elder's",
+]
+QUALITY_TIERS = [
+    "Normal",
+    "Good",
+    "Outstanding",
+    "Excellent",
+    "Masterpiece",
+]
+CITY_COLOURS = {
+    "Thetford": "purple",
+    "Martlock": "skyblue",
+    "Caerleon": "red",
+    "Lymhurst": "green",
+    "Bridgewatch": "orange",
+    "Fort Sterling": "grey",
+    "Black Market": "white",
+    "Arthurs Rest": "dodgerblue",
+    "Merlyns Rest": "lawngreen",
+    "Morganas Rest": "midnightblue",
+}
